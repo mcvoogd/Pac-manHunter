d4df3b7f20583efa4ef14778bdf5b1cdf0fd4fd1
HarmenL <leusinkharmen@gmail.com>
2016-05-19 17:16:59 +0200

    Fixed

--- src/client/Panel.java
+++ src/client/Panel.java
@@ -18,6 +18,7 @@ public class Panel extends JPanel implements ActionListener, KeyListener{
     public Panel(Client client){
         character = new Character();
         player2 = new Character();
+        player2.setColor(Color.RED);
         timer = new Timer(1000/60, this);
         this.client = client;
         addKeyListener(this);
